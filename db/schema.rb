13e900bccb92df44846cb963d6f8a77d68affd6a
Grzegorz Wiktorzak <wiktorzak.greg@gmail.com>
2021-03-02 18:23:53 +0100

    booking mvc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_160957) do
+ActiveRecord::Schema.define(version: 2021_03_02_164359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,23 @@ ActiveRecord::Schema.define(version: 2021_03_02_160957) do
     t.integer "number_of_members"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.boolean "confirmation"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "room_socials", force: :cascade do |t|
+    t.string "type"
+    t.string "profile_url"
+    t.bigint "room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_room_socials_on_room_id"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -51,5 +68,6 @@ ActiveRecord::Schema.define(version: 2021_03_02_160957) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "room_socials", "rooms"
   add_foreign_key "rooms", "users"
 end
