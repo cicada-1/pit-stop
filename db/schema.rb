65884b8cd8b86c2ee8f2d37c242ced443c397666
Peter Wallace <peterwallace@hotmail.co.uk>
2021-03-02 16:13:31 +0000

    Run migration file for band model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_124531) do
+ActiveRecord::Schema.define(version: 2021_03_02_160957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bands", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.text "description"
+    t.integer "number_of_members"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
