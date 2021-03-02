cd5b410af548352a17a40120acab53a2958f47ae
Peter Wallace <peterwallace@hotmail.co.uk>
2021-03-02 16:45:37 +0000

    Run migration file for new columns in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_160957) do
+ActiveRecord::Schema.define(version: 2021_03_02_164021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2021_03_02_160957) do
     t.integer "number_of_members"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "room_socials", force: :cascade do |t|
+    t.string "type"
+    t.string "profile_url"
+    t.bigint "room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_room_socials_on_room_id"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -47,9 +56,14 @@ ActiveRecord::Schema.define(version: 2021_03_02_160957) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "type"
+    t.string "stage_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "room_socials", "rooms"
   add_foreign_key "rooms", "users"
 end
