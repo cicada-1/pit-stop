450644de95c4b79d694bab5df792632a9e78d84f
Grzegorz Wiktorzak <67414488+GWiktorzak@users.noreply.github.com>
2021-03-03 16:48:06 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2021_03_03_112911) do
 
-ActiveRecord::Schema.define(version: 2021_03_02_164801) do
 
 
   # These are extensions that must be enabled in order to support this database
