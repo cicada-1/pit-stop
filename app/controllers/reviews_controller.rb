884a52103f29d3381864369d45cb6ca2577e78f4
Peter Wallace <peterwallace@hotmail.co.uk>
2021-03-04 22:48:56 +0000

    Created preliminary #create method in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,18 @@
 class ReviewsController < ApplicationController
   def create
+    if params[:room_id]
+      @room = Room.find(params[:room_id])
+      @band = Band.where("name ILIKE ?", "%#{params[:band]}%").select { |element| element.band_members.find_by(user_id: current_user.id) }[0]
+      @review = RoomSocial.new(review_params.merge(room_id: @room.id, band_id: @band.id))
+      authorize @room
+      authorize @band
+      if @review.save
+        redirect_to room_path(@room), notice: "Your review has been added!"
+      else
+        render "rooms/show"
+      end
+    else
+
   end
 
   def update
