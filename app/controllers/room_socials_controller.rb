8d1f5e852972e1ed1b4c937c11cc804c00ad9a5b
Peter Wallace <peterwallace@hotmail.co.uk>
2021-03-04 12:07:55 +0000

    Write #create method for room_socials controller. Fixed typos in room_social and band_social validations

--- app/controllers/room_socials_controller.rb
+++ app/controllers/room_socials_controller.rb
@@ -1,5 +1,13 @@
 class RoomSocialsController < ApplicationController
   def create
+    @room = current_user.rooms.new
+    authorize @room
+    @room_social = RoomSocial.new(room_social_params.merge(room_id: @room.id))
+    if @room_social.save
+      redirect_to room_path(@room), notice: "#{@room_social.social_type} url has been added!"
+    else
+      render "rooms/show"
+    end
   end
 
   def destroy
