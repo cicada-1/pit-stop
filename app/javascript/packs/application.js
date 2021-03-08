202138bbaef26bbf9845db77fcfbdc1d85cda650
Ed Melech <mr.melech@gmail.com>
2021-03-08 09:57:16 +0000

    navbar and footer updated

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,6 +28,6 @@ import { initMapbox } from '../plugins/init_mapbox';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-   initMapbox();
+    initMapbox();
 
 });
